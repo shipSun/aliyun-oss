ea4a89a8152482e1b7383809b8f9dbae23a73590
shipSun <543999860@qq.com>
2018-01-23 00:49:07 +0800

    init

---
+++ oss-sdk.js
@@ -0,0 +1,32 @@
+function OssSdk(){
+    var client;
+
+    return function(){
+        this.initClient(data);
+    }
+}
+OssSdk.prototype.initClient = function (data) {
+
+}
+OssSdk.prototype.initRequest = function (){
+
+}
+OssSdk.prototype.initFunc = function (data) {
+
+}
+OssSdk.prototype.upload = function () {
+
+}
+OssSdk.prototype.download = function () {
+
+}
+OssSdk.prototype.progress = function (p) {
+
+}
+OssSdk.prototype.endFunc = function (data) {
+    
+}
+
+
+var OssSdk = new OssSdk();
+OssSdk.upload();
---
+++ restful.js
@@ -0,0 +1,125 @@
+/**
+ * @author shipSun
+ */
+function successHandle(data){
+	$.restful.isLoginHandle(data);
+	$.restful.success(data);
+}
+function RestFul(){
+	this.type="get";
+	this.url="";
+	this.data="";
+	this.async=true;
+	this.debug = false;
+
+	this.ajax=function(){
+		$.ajax({
+			"type":this.type,
+			"url":this.url,
+			"async":this.async,
+			"cache":true,
+			"complete":this.complete,
+			"error":this.error,
+			"success":successHandle,
+			"contentType":"application/x-www-form-urlencoded",
+			"data":this.data,
+			"dataType":"json"
+		});
+	};
+	this.complete=function(xhr, type){
+		if(this.debug) {
+            console.log('执行完成', xhr, type);
+        }
+	};
+	this.error=function(xhr,errStr){
+        if(this.debug) {
+            console.error('出错', xhr, errStr);
+        }
+		alert(errStr);
+	};
+    this.success = function(data){
+        if(this.debug) {
+            console.log('执行成功', data);
+        }
+    }
+    this.isLoginHandle = function(data){
+        if(this.debug) {
+            console.error('登录失败', data);
+
+        }else{
+            if(data.code==403){
+            	window.location.href=data.location;
+            }
+		}
+    }
+}
+RestFul.prototype.init = function(config){
+	if(config.async!==undefined){
+		this.async = config.async;
+	}
+	if(config.debug!==undefined){
+		this.debug = config.debug;
+	}
+	return this;
+}
+RestFul.prototype.post = function(url, data, success, error, complete){
+	this.url=url;
+	this.data=data;
+	this.type='POST';
+	if(success){
+		this.success=success;
+	}
+	if(error){
+		this.error = error;
+	}
+	if(complete){
+		this.complete=complete;
+	}
+	this.ajax();
+};
+RestFul.prototype.get = function(url, success, error, complete){
+	this.url=url;
+	this.data='';
+	this.type='GET';
+	if(success){
+		this.success=success;
+	}
+	if(error){
+		this.error = error;
+	}
+	if(complete){
+		this.complete=complete;
+	}
+	this.ajax();
+};
+RestFul.prototype.put = function(url, data, success, error, complete){
+	this.url=url;
+	this.data=data;
+	this.type='PUT';
+	if(success){
+		this.success=success;
+	}
+	if(error){
+		this.error = error;
+	}
+	if(complete){
+		this.complete=complete;
+	}
+	this.ajax();
+};
+RestFul.prototype.del = function(url, success, error, complete){
+	this.url=url;
+	this.data='';
+	this.type='DELETE';
+	if(success){
+		this.success=success;
+	}
+	if(error){
+		this.error = error;
+	}
+	if(complete){
+		this.complete=complete;
+	}
+	this.ajax();
+};
+jQuery.restful = new RestFul().init({});
